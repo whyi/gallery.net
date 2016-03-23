01faa1c1eef1d7c4d2989208d8fa47be6e15d605
Jeonggyu Lee <glaze@whyi.net>
2016-03-22 20:38:58 -0700

    Initial bootstraping

---
+++ Gallery/src/Gallery/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gallery.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
